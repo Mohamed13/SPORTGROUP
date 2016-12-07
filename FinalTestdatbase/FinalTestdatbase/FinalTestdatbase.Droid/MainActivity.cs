1bccd5f7016ff2b43bf9994bc8475b3c5bb20864
Alexandre83 <alexandre.vignon@ynov.com>
2016-12-07 11:39:35 +0100

    Changement BDD
    Nouvelle BDD freemysqlhosting.net

--- FinalTestdatbase/FinalTestdatbase/FinalTestdatbase.Droid/MainActivity.cs
+++ FinalTestdatbase/FinalTestdatbase/FinalTestdatbase.Droid/MainActivity.cs
@@ -55,7 +55,7 @@ namespace FinalTestdatbase.Droid
             StartActivity(typeof(Register));
         }
 
-        MySqlConnection con = new MySqlConnection("Server=sql7.freemysqlhosting.net;Port=3306;database=sql7147202; User Id=sql7147202;Password=TQ13I75cK3;");
+        MySqlConnection con = new MySqlConnection("Server=sql7.freemysqlhosting.net;Port=3306;database=sql7148210; User Id=sql7148210;Password=msvYc7aw45;");
         
         
         private void button1_Click(object sender, EventArgs e)
